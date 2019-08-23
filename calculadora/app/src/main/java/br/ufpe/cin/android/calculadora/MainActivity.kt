4389b39cd3aad5e36c3503fbf8761fcf2a9f6e62
Michael barney Junior <michaelbarneyjunior@gmail.com>
2019-08-23 09:49:50 -0300

    added equals and error toast

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,14 +2,80 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+//Import all of the widgets
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
+
+
+// Creathe the class as an OnClick Listener
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add on click listeners for all the buttons
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_Add.setOnClickListener(this);
+        btn_Clear.setOnClickListener(this);
+        btn_Divide.setOnClickListener(this);
+        btn_Dot.setOnClickListener(this);
+        btn_Equal.setOnClickListener(this);
+        btn_LParen.setOnClickListener(this);
+        btn_Multiply.setOnClickListener(this);
+        btn_Power.setOnClickListener(this);
+        btn_RParen.setOnClickListener(this);
+        btn_Subtract.setOnClickListener(this);
+
     }
 
+    override fun onClick(v: View?) {
+        when (v) {
+            // Botoões que somente escrevem
+            btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_Add, btn_Divide, btn_Dot, btn_LParen, btn_Multiply, btn_Power, btn_RParen, btn_Subtract -> {
+                text_calc.setText(text_calc.text.toString() + (v as Button).text.toString())
+            }
+
+            //Botão e Apagar
+            btn_Clear -> {
+                var text = text_calc.text.toString();
+                text = text.substring(0, text.length - 1);
+                text_calc.setText(text)
+            }
+
+            //Botão de escrever
+            btn_Equal -> {
+                print("EQUAL")
+                var text = text_calc.text.toString();
+
+                // Try Catch to see if the expression is valid
+                try {
+                    // If it is, show it!
+                    text_info.text = eval(text).toString();
+                }
+                catch (e: Throwable) {
+                    // If not, show a Toast
+                    Toast.makeText(this, "Expressão Inválida", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            else -> {
+            }
+        }
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
